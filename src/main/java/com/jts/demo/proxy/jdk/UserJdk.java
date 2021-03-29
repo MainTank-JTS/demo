628a21d3690441088fcf3398af0aae4b34ba31c1
MainTank <maintank2020@126.com>
2021-03-29 14:06:40 +0800

    update UserJdk newUser method

--- src/main/java/com/jts/demo/proxy/jdk/UserJdk.java
+++ src/main/java/com/jts/demo/proxy/jdk/UserJdk.java
@@ -2,31 +2,32 @@ package com.jts.demo.proxy.jdk;
 
 import com.jts.demo.proxy.IUserOp;
 import com.jts.demo.proxy.User;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Objects;
 
 @Slf4j
 @Component
 public class UserJdk implements InvocationHandler {
 
-    private IUserOp target;
+    private Object target;
 
-    public void bind(IUserOp target){
+    public void bind(@NonNull Object target){
         this.target = target;
     }
 
     public IUserOp newUser(){
-        return (IUserOp) Proxy.newProxyInstance(target.getClass().getClassLoader(), new Class[]{IUserOp.class},this);
+        return (IUserOp) Proxy.newProxyInstance(IUserOp.class.getClassLoader(), new Class[]{IUserOp.class},this);
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         log.info("Use [{}],invock [{}]", UserJdk.class.getName(),method.getName());
-        Object res = method.invoke(target,args);
-        return res;
+        return method.invoke(target,args);
     }
 }
